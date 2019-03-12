cb60cbab4857f961ca9eb0160b02f31720036846
mdsteiner <markus.steiner@unibas.ch>
2019-03-12 16:28:22 +0100

    updated pilot analysis

--- r/pilot/2_pilot_analyses.R
+++ r/pilot/2_pilot_analyses.R
@@ -1,5 +1,6 @@
 # Anlysis Script Pilot Study
 library(tidyverse)
+library(patchwork)
 library(psych)
 library(tidytext)
 library(rstanarm)
@@ -168,29 +169,150 @@ ggplot(pilot, aes(x = m_sentiment)) +
 # overall correlation
 cor(pilot$rating, pilot$med_risk, method = "spearman")
 
+pilot_long <- pilot_long %>%
+  mutate(n_aspects = n_seeking + n_avoiding,
+         n_aspects_cat = case_when(n_aspects == 1 ~ "1 Aspect",
+                                   n_aspects == 2 ~ "2 Aspects",
+                                   n_aspects == 3 ~ "3 Aspects",
+                                   n_aspects == 4 ~ "4 Aspects",
+                                   n_aspects == 5 ~ "5 Aspects",
+                                   n_aspects >= 6 ~ "6+ Aspects"))
+
 # correlation dataset with correlations as a function of aspect position
 cors <- pilot_long %>%
-  group_by(aspect_ind_cat) %>%
+  group_by(aspect_ind_cat, n_aspects_cat) %>%
   summarise(
     cors = cor(r_risk, rating, use = "pairwise", method = "spearman"),
     N = n()
   )
 
+# show n participants per row
+cors %>% group_by(n_aspects_cat) %>% summarise(nn = N[1])
+
 ggplot(pilot_long, aes(x = r_risk, y = rating)) +
   geom_point(col = "#8c9196") +
-  geom_smooth(method = "lm", se = FALSE) +
-  geom_label(data = cors, aes(label = paste0("rho = ", round(cors, 2), "; N = ", N)), 
-             x = -Inf, y = Inf, hjust = -0.2, vjust = 1.2)+
-  facet_wrap(~ aspect_ind_cat, ncol = 3) +
+  geom_smooth(method = "lm", se = FALSE, col = "blue") +
+  geom_label(data = cors, aes(label = paste0("rho = ", round(cors, 2))), 
+             x = 3, y = 10.1)+
+  facet_grid(n_aspects_cat ~ aspect_ind_cat) +
   theme_bw() +
   labs(
-    x = "Aspect's strength of evidence",
-    y = "Risk-taking propensity"
+    x = "Aspect's Strength of Evidence",
+    y = "Risk-Taking Propensity"
   ) + 
+  ylim(-.1, 11) +
   theme(
     strip.text = element_text(size = 12, face = "bold"),
     axis.text = element_text(size = 12),
-    axis.title = element_text(size = 16,face = "bold")
+    axis.title = element_text(size = 16,face = "bold"),
+    strip.background = element_rect(fill="lightgrey")
   )
 
-ggsave("plots/pilot/evidencestrength_propensity_corrs.pdf", width = 8, height = 6)
+ggsave("plots/pilot/evidencestrength_propensity_corrs_grid.pdf",
+       width = 14, height = 8)
+
+
+### Variation in Ratings ===========================
+
+# correlation dataset with correlations as a function of aspect position
+p_risk <- pilot_long %>% 
+  filter(n_aspects_cat != "1 Aspect") %>%
+  mutate(aspect_ind_cat = substr(aspect_ind_cat, 8, 9)) %>%
+  ggplot(aes(x = aspect_ind_cat, y = r_risk, group = partid)) +
+  geom_point(alpha = .2) + 
+  geom_line(alpha = .2) +
+  stat_summary(fun.y = "mean", geom = "point", size = 3, group = 1) +
+  stat_summary(fun.data = mean_sdl, geom = "errorbar", fun.args = list(mult=1),
+               group = 1) +
+  facet_wrap( ~ n_aspects_cat, ncol = 5, scales = "free_x") +
+  theme_bw() +
+  labs(
+    x = "",
+    y = "Risk"
+  ) +
+  theme(
+    strip.text = element_text(size = 12, face = "bold"),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 16,face = "bold"),
+    strip.background = element_rect(fill="lightgrey")
+  )
+
+
+
+# correlation dataset with correlations as a function of aspect position
+p_social <- pilot_long %>% 
+  filter(n_aspects_cat != "1 Aspect") %>%
+  mutate(aspect_ind_cat = substr(aspect_ind_cat, 8, 9)) %>%
+  ggplot(aes(x = aspect_ind_cat, y = r_social, group = partid)) +
+  geom_point(alpha = .2) + 
+  geom_line(alpha = .2) +
+  stat_summary(fun.y = "mean", geom = "point", size = 3, group = 1) +
+  stat_summary(fun.data = mean_sdl, geom = "errorbar", fun.args = list(mult=1),
+               group = 1) +
+  facet_wrap( ~ n_aspects_cat, ncol = 5, scales = "free_x") +
+  theme_bw() +
+  labs(
+    x = "",
+    y = "Social"
+  ) +
+  theme(
+    strip.text = element_text(size = 12, face = "bold"),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 16,face = "bold"),
+    strip.background = element_rect(fill="lightgrey")
+  )
+
+# correlation dataset with correlations as a function of aspect position
+p_situation <- pilot_long %>% 
+  filter(n_aspects_cat != "1 Aspect") %>%
+  mutate(aspect_ind_cat = substr(aspect_ind_cat, 8, 9)) %>%
+  ggplot(aes(x = aspect_ind_cat, y = r_situation, group = partid)) +
+  geom_point(alpha = .2) + 
+  geom_line(alpha = .2) +
+  stat_summary(fun.y = "mean", geom = "point", size = 3, group = 1) +
+  stat_summary(fun.data = mean_sdl, geom = "errorbar", fun.args = list(mult=1),
+               group = 1) +
+  facet_wrap( ~ n_aspects_cat, ncol = 5, scales = "free_x") +
+  theme_bw() +
+  labs(
+    x = "",
+    y = "Situation"
+  ) +
+  theme(
+    strip.text = element_text(size = 12, face = "bold"),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 16,face = "bold"),
+    strip.background = element_rect(fill="lightgrey")
+  )
+
+
+# correlation dataset with correlations as a function of aspect position
+p_frequency <- pilot_long %>% 
+  filter(n_aspects_cat != "1 Aspect") %>%
+  mutate(aspect_ind_cat = substr(aspect_ind_cat, 8, 9)) %>%
+  ggplot(aes(x = aspect_ind_cat, y = r_frequency, group = partid)) +
+  geom_point(alpha = .2) + 
+  geom_line(alpha = .2) +
+  stat_summary(fun.y = "mean", geom = "point", size = 3, group = 1) +
+  stat_summary(fun.data = mean_sdl, geom = "errorbar", fun.args = list(mult=1),
+               group = 1) +
+  facet_wrap( ~ n_aspects_cat, ncol = 5, scales = "free_x") +
+  theme_bw() +
+  labs(
+    x = "Aspect Position",
+    y = "Frequency"
+  ) +
+  theme(
+    strip.text = element_text(size = 12, face = "bold"),
+    axis.text = element_text(size = 12),
+    axis.title = element_text(size = 16,face = "bold"),
+    strip.background = element_rect(fill="lightgrey")
+  )
+
+
+pdf("plots/pilot/ratings_variation.pdf",
+    width = 12, height = 6)
+
+p_risk + p_social + p_situation + p_frequency + plot_layout(ncol = 1)
+dev.off()
+
